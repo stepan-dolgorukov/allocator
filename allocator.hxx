67cd083e5a0e47bd04d72f6af8b9f7db014f4b45
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2025-03-11 10:52:20 +0500

    Уход от применения оператора "decltype" (учёт замечания)

--- allocator.hxx
+++ allocator.hxx
@@ -14,14 +14,14 @@ class allocator {
 private:
   std::size_t                                          _size_block{}, _blocks_amount{}, _size_storage{};
   std::byte *                                          _storage{};
-  decltype( _storage )                                 _pointer{};
+  std::byte *                                          _pointer{};
   std::unordered_map< std::byte *, const std::size_t > _left_borders{};
   std::mutex                                           _mutex{};
 
 public:
   allocator( std::size_t size_block = 1u, std::size_t blocks_amount = 1u );
   ~allocator();
-  auto take( std::size_t blocks_amount ) -> decltype( _storage );
+  auto take( std::size_t blocks_amount ) -> std::byte *;
   auto release( std::byte * block_pointer ) -> void;
 };
 
